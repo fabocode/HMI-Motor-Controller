0a869dfe786d447d681f7275dc88a304b190301b
fabocode <fabianbarraez@gmail.com>
2021-11-22 16:39:56 -0500

    test: 💍 setting up a simple test to read ADC

--- test/adc.py
+++ test/adc.py
@@ -2,4 +2,21 @@ import piplates.DAQC2plate as DAQC2
 import time
 import sys
 
-DAQC2.getADC(0,0)
+if __name__ == '__main__':
+    try:
+
+        # Initialize the DAQC2
+        # DAQC2.ADC_init()
+        # # Set the ADC to read from the internal temperature sensor
+        # DAQC2.ADC_set_temp_sensor(1)
+        # # Set the ADC to read from the internal temperature sensor
+        # DAQC2.ADC_set_temp_sensor(2)
+        # Set the ADC to read from the internal temperature sensor
+        adc_reading = DAQC2.getADC(0,0)
+        print(f"Torque sensor output: {adc_reading}")
+        time.sleep(1)
+
+    except KeyboardInterrupt:
+        print("\n\nProgram terminated by user. Exiting now...\n\n")
+        sys.exit(0)
+        
