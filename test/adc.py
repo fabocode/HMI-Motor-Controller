4b451c84def720fef7e2888cf1b0928d8bc54362
fabocode <fabianbarraez@gmail.com>
2021-11-22 17:39:04 -0500

    refactor: 💡 remove comments not used

--- test/adc.py
+++ test/adc.py
@@ -5,16 +5,9 @@ import sys
 if __name__ == '__main__':
     try:
         while True:
-            # Initialize the DAQC2
-            # DAQC2.ADC_init()
-            # # Set the ADC to read from the internal temperature sensor
-            # DAQC2.ADC_set_temp_sensor(1)
-            # # Set the ADC to read from the internal temperature sensor
-            # DAQC2.ADC_set_temp_sensor(2)
-            # Set the ADC to read from the internal temperature sensor
             adc_reading = DAQC2.getADC(0,0)
             print(f"Torque sensor output: {adc_reading}")
-            time.sleep(1)
+            time.sleep(.5)  # read every half second
 
     except KeyboardInterrupt:
         print("\n\nProgram terminated by user. Exiting now...\n\n")
