f517b50d5a131ac68dc8ca0ecdfe34701f90d1a2
fabocode <fabianbarraez@gmail.com>
2021-11-22 16:42:01 -0500

    test: 💍 add infinite loop to read multiple readings

--- test/adc.py
+++ test/adc.py
@@ -4,19 +4,18 @@ import sys
 
 if __name__ == '__main__':
     try:
-
-        # Initialize the DAQC2
-        # DAQC2.ADC_init()
-        # # Set the ADC to read from the internal temperature sensor
-        # DAQC2.ADC_set_temp_sensor(1)
-        # # Set the ADC to read from the internal temperature sensor
-        # DAQC2.ADC_set_temp_sensor(2)
-        # Set the ADC to read from the internal temperature sensor
-        adc_reading = DAQC2.getADC(0,0)
-        print(f"Torque sensor output: {adc_reading}")
-        time.sleep(1)
+        while True:
+            # Initialize the DAQC2
+            # DAQC2.ADC_init()
+            # # Set the ADC to read from the internal temperature sensor
+            # DAQC2.ADC_set_temp_sensor(1)
+            # # Set the ADC to read from the internal temperature sensor
+            # DAQC2.ADC_set_temp_sensor(2)
+            # Set the ADC to read from the internal temperature sensor
+            adc_reading = DAQC2.getADC(0,0)
+            print(f"Torque sensor output: {adc_reading}")
+            time.sleep(1)
 
     except KeyboardInterrupt:
         print("\n\nProgram terminated by user. Exiting now...\n\n")
         sys.exit(0)
-        
