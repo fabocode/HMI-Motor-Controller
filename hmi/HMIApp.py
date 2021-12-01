97fdefea1460223ca39f045ccf6c7f720ccc3885
Fabian <fabianbarraez@gmail.com>
2021-12-01 09:19:53 -0500

    feat: add kv files

--- hmi/HMIApp.py
+++ hmi/HMIApp.py
@@ -2,6 +2,10 @@ import kivy
 kivy.require('2.0.0') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
+from kivy.properties import StringProperty
 from kivy.config import Config
 from screeninfo import get_monitors
 from HMIConfig import HMI_Config
@@ -16,11 +20,22 @@ screen_height = str(int(get_monitors()[0].height * config.get_screen_fixed_size(
 Config.set('graphics', 'width', screen_width)
 Config.set('graphics', 'height', screen_height)    
 
-class MyApp(App):
+# main screen
+class Main(Screen):
+    pass
+
+# screen manager
+class WindowManager(ScreenManager):
+    pass
+
+# designate our .kv design file 
+kv = Builder.load_file("hmi.kv")
+
+
+class HMIApp(App):
 
     def build(self):
-        return Label(text='Hello world')
-
+        return kv
 
 if __name__ == '__main__':
-    MyApp().run()
+    HMIApp().run()
