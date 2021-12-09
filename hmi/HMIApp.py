1568ce6c3f096538facc46790f69792241603ae1
Fabian <fabianbarraez@gmail.com>
2021-12-08 19:52:27 -0500

    feat: add excel generation file function call (needs improvement)

--- hmi/HMIApp.py
+++ hmi/HMIApp.py
@@ -9,10 +9,11 @@ from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 from kivy.properties import ListProperty
 from kivy.config import Config
 from screeninfo import get_monitors
-from datetime import date, timedelta
+from datetime import date, datetime
 from timeit import default_timer as timer
 from HMIConfig import HMI_Config
 import time
+import excel
 
 # load the configuration file
 config = HMI_Config('config/hmi.yaml')
@@ -35,18 +36,40 @@ class Main(Screen):
         super(Main, self).__init__(**kwargs)    # call the super class constructor (Screen)
         self.system_status  = False
         self.run_button_str = "START"
-        self.date_str       = str(date.today().strftime("%d/%m/%y"))  # update the date string
+        self.date_str       = self.get_date()  # update the date string
         self.status_bar_str = "STOPPED"
         self.test_name_str  = "Test Name"
         self.timestamp_str  = str("00:00:00")
         self.start = timer()
         self.end = timer()
+        self.excel = excel # create an instance of the excel module to save the data
         Clock.schedule_interval(self.update_callback, 0.5)    # setup periodic task
-        Clock.schedule_interval(self.update_callback_date, 60)    # setup periodic task
+        Clock.schedule_interval(self.update_callback_date, 300)    # setup periodic task
+
+        self.data = {   # create a dictionary to store the data
+            'Start Time': (),
+            'Stop Time': (),
+            'Elapsed Time': (),
+            'Time Stamps': (),
+            'RPM': (),
+            'Torque': (),
+            'Blade Tip Velocity': ()
+        }
+
+    # Utils functions
+    def get_date(self):
+        return str(date.today().strftime("%d/%m/%y"))
+
+    def get_time(self):
+        return str(datetime.now().strftime("%H:%M:%S - %d/%m/%y"))
     
     def get_elapsed_time(self):
-        return time.strftime("%H:%M:%S", time.gmtime(self.start - self.end))	
+        return str(time.strftime("%H:%M:%S", time.gmtime(self.start - self.end))	)
 
+    def is_system_running(self):
+        return self.system_status
+
+    # GUI functions and events
     def control_status_bar(self):
         if self.is_system_running():    # if system is running and no faults are detected
             self.status_bar_str = "RUNNING"
@@ -56,31 +79,60 @@ class Main(Screen):
             self.end = timer()
             self.start = timer()
 
-    def update_callback(self, dt):
-        
-        self.control_status_bar()   # update the status bar
-        if self.is_system_running():    # if system is running and no faults are detected
-            self.timestamp_str = str(self.get_elapsed_time())
-
-    def update_callback_date(self, dt):
-        self.date_str = str(date.today().strftime("%d/%m/%y"))  # update the date string
-
     def on_test_name_evt(self, text_input):
         print(f"text included - {text_input}")
         self.test_name_str = text_input
 
-    def is_system_running(self):
-        return self.system_status
+    def clear_data(self):
+        self.data = {   # create a dictionary to store the data
+            'Start Time': (),
+            'Stop Time': (),
+            'Elapsed Time': (),
+            'Time Stamps': (),
+            'RPM': (),
+            'Torque': (),
+            'Blade Tip Velocity': ()
+        }
 
     def run_button_pressed(self):
         self.system_status = not self.system_status
         if self.system_status:
             self.run_button_str = 'STOP'
             self.ids['run_button_id'].background_color = [1, 0, 0, 1]
+
+            self.data['Start Time'] += (self.get_time(),)
         else:
+            # if test name is not empty, save the data to the excel file
+            if self.test_name_str != '' and self.test_name_str != 'Test Name' and self.data['Time Stamps'] != ():
+                self.excel.save_data(self.data, self.test_name_str)
+                # save data into excel file 
+                print("saving data into excel file ")
+                self.excel.save_data(self.data, self.test_name_str)
+                self.clear_data()
             self.run_button_str = 'START'
             self.ids['run_button_id'].background_color = [0, 1, 0, 1]
-        
+            self.data['Stop Time'] += (self.get_time(),)
+    
+    # Callback functions for the periodic task
+
+    def update_callback(self, dt):
+    
+        self.control_status_bar()   # update the status bar
+        if self.is_system_running():    # if system is running and no faults are detected
+            self.timestamp_str = self.get_elapsed_time()    # update the timestamp string
+
+            # self.data['Start Time'].append(self.get_time())
+            # self.data['Stop Time'] = self.get_time()
+            self.data['Elapsed Time'] += (self.get_elapsed_time(),)
+            self.data['Time Stamps'] += (self.get_time(),)
+            self.data['RPM'] += (0,)
+            self.data['Torque'] += (0,)
+            self.data['Blade Tip Velocity'] += (0,)
+
+    # callback function for the date update    
+    def update_callback_date(self, dt):
+        self.date_str = str(date.today().strftime("%d/%m/%y"))  # update the date string
+
 
 # screen manager
 class WindowManager(ScreenManager):
---
+++ hmi/excel.py
@@ -0,0 +1,53 @@
+import xlsxwriter
+
+def save_data(data, filename):
+    file_path_name = filename + '.xlsx'
+    workbook = xlsxwriter.Workbook(file_path_name)    # Create an new Excel file and add a worksheet.
+    worksheet = workbook.add_worksheet()
+
+    # Widen the first column to make the text clearer.
+    worksheet.set_column('A:A', 20)
+    worksheet.set_column('B:B', 20)
+    worksheet.set_column('C:C', 20)
+    worksheet.set_column('D:D', 20)
+    worksheet.set_column('E:E', 20)
+    worksheet.set_column('F:F', 20)
+    worksheet.set_column('G:G', 20)
+
+    # Add a bold format to use to highlight cells.
+    header_format = workbook.add_format({'bold': True, 'text_wrap': True, 'align': 'center', 'valign': 'vcenter'})
+
+    # Write some data headers.
+    worksheet.write('A1', 'Name', header_format)
+
+    # Write some simple text.
+    worksheet.write('A1', 'Start Time', header_format)
+    worksheet.write('B1', 'Stop Time', header_format)
+    worksheet.write('C1', 'Elapsed Time', header_format)
+    worksheet.write('D1', 'Time Stamps', header_format) # on each line of measurement (1 second each recording)
+    worksheet.write('E1', 'RPM', header_format)
+    worksheet.write('F1', 'Torque', header_format)
+    worksheet.write('G1', 'Blade Tip Velocity', header_format)
+
+    # write data dictionary into excel 
+    # dict = {'Name': '', 'Start Time': '', 'Stop Time': '', 'Elapsed Time': '', 'Time Stamps': '', 'RPM': '', 'Torque': '', 'Blade Tip Velocity': ''}
+    row = 1
+    col = 0
+    
+    # write each value tuple index into excel
+    for key, value in data.items():
+        for i in range(len(value)):
+            # worksheet.write(row, col, key)
+            # row += 1
+            worksheet.write(row, col, value[i])
+            row += 1
+            
+        row = 1
+        col += 1
+    
+
+    
+    print(f"save data here {data}")
+
+    workbook.close()
+    
