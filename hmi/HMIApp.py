054cd5976adc893a314fc75ad931723c7cf25a9d
Fabian <fabianbarraez@gmail.com>
2021-12-10 09:29:27 -0500

    fix: always getting torque sensor data

--- hmi/HMIApp.py
+++ hmi/HMIApp.py
@@ -130,10 +130,10 @@ class Main(Screen):
     def update_callback(self, dt):
         self.control_status_bar()   # update the status bar
         torque_data = self.torque_sensor.get_torque()
+        self.torque_sensor_str = str(torque_data)
         print(f"torque data - {torque_data}")
         if self.is_system_running():    # if system is running and no faults are detected
             self.timestamp_str = self.get_elapsed_time()    # update the timestamp string
-            self.torque_sensor_str = str(torque_data)
             self.data['Elapsed Time'].append(self.get_elapsed_time())
             self.data['Time Stamps'].append(self.get_time())
             self.data['RPM'].append(0)
