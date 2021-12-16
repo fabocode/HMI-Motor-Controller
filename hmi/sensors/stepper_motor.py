cd0f9400a0685febf823e3dba17518935ba59b44
fabocode <fabianbarraez@gmail.com>
2021-12-16 10:55:26 -0400

    test: 💍 test driving stepper motor

--- hmi/sensors/stepper_motor.py
+++ hmi/sensors/stepper_motor.py
@@ -13,10 +13,21 @@ class Stepper_Motor:
             print("Error: Frequency sensor not connected")
             return 0.0
 
+    def set_motor(self) -> bool:
+        try:
+            DAQC2.setDOUT(1, 7)
+            return True 
+        except:
+            print("error writing to motor prin")
+            return False
+
+
 if __name__ == '__main__':
     motor = Stepper_Motor()
     while True:
         freq = motor.get_frequency()
         print(f"freq: {freq}")
+        print("set the motor")
+        motor.set_motor()
         print("")
         time.sleep(1)
