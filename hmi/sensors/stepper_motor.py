50952059d1081766e470066f3931b579a1b593f0
fabocode <fabianbarraez@gmail.com>
2021-12-17 09:48:55 -0400

    test: 💍 testing motor behavior with software

--- hmi/sensors/stepper_motor.py
+++ hmi/sensors/stepper_motor.py
@@ -13,14 +13,21 @@ class Stepper_Motor:
             print("Error: Frequency sensor not connected")
             return 0.0
 
-    def set_motor(self) -> bool:
-        # try:
-        DAQC2.clrDOUTbit(0, 0)
-        return True 
-        # except:
-        #     print("error writing to motor prin")
-        #     return False
+    def turn_on_motor(self) -> bool:
+        try:
+            DAQC2.setDOUTbit(0, 0)
+            return True 
+        except:
+            print("error writing to motor prin")
+            return False
 
+    def turn_off_motor(self) -> bool:
+        try:
+            DAQC2.clrDOUTbit(0, 0)
+            return True
+        except:
+            print("error writing to motor prin")
+            return False
 
 if __name__ == '__main__':
     motor = Stepper_Motor()
@@ -28,6 +35,7 @@ if __name__ == '__main__':
         freq = motor.get_frequency()
         print(f"freq: {freq}")
         print("set the motor")
-        motor.set_motor()
-        print("")
-        time.sleep(1)
+        motor.turn_on_motor()
+        time.sleep(.1)
+        motor.turn_off_motor()
+        time.sleep(.1)
