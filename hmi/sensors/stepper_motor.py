37f9870031ef2b19f5c65ced53974efafdf9150b
fabocode <fabianbarraez@gmail.com>
2021-12-18 18:14:29 -0400

    feat: 🎸 add motor frequency setup

--- hmi/sensors/stepper_motor.py
+++ hmi/sensors/stepper_motor.py
@@ -2,9 +2,10 @@ import piplates.DAQC2plate as DAQC2
 import time 
 
 class Stepper_Motor:
-    def __init__(self, addr=0):
+    def __init__(self, dir_pin=0, addr=0, channel=0):
         self.addr = addr
-        self.channel = 0
+        self.dir_pin = dir_pin
+        self.channel = channel
     
     def get_frequency(self):
         try:
@@ -12,6 +13,23 @@ class Stepper_Motor:
         except:
             print("Error: Frequency sensor not connected")
             return 0.0
+
+    def set_direction(self, direction: bool) -> bool:
+        try:
+            if direction:
+                DAQC2.setDOUTbit(0, self.dir_pin)
+            else:
+                DAQC2.clrDOUTbit(0, self.dir_pin)
+            return True
+        except:
+            print("error writing to motor prin")
+            return False
+
+    def set_clockwise(self):
+        self.set_direction(True)
+    
+    def set_counter_clockwise(self):
+        self.set_direction(False)
 
     def set_motor(self, freq):
         try:
@@ -21,28 +39,12 @@ class Stepper_Motor:
             print("error writing to motor prin")
             return False
 
-    def on(self) -> bool:
-        # try:
-        DAQC2.clrDOUTbit(0, 0)
-        return True 
-        # except:
-        #     print("error writing to motor prin")
-        #     return False
-
-    def off(self):
-        # try:
-        DAQC2.setDOUTbit(0, 0)
-        return True
-        # except:
-        #     print("error writing to motor prin")
-        #     return False
-
 if __name__ == '__main__':
     motor = Stepper_Motor()
+    motor.set_clockwise()
+    motor.set_motor(300)
     while True:
         freq = motor.get_frequency()
         print(f"freq: {freq}")
         print("set the motor")
-        print("")
-        motor.set_motor(512)
-        time.sleep(.1)
+        time.sleep(1)
