90cd0b9350690144e7210d045890c4906c9076d7
fabocode <fabianbarraez@gmail.com>
2021-12-21 17:00:50 -0400

    test: 💍 testing motor on feature

--- hmi/sensors/stepper_motor.py
+++ hmi/sensors/stepper_motor.py
@@ -13,9 +13,17 @@ class Stepper_Motor:
         self.freq = 10
         self.type = 3   # square wave
         self.level = 4  # 1:1
-        self.initFG()
+        DAQC2.fgTYPE(self.addr, self.channel, self.type)
+        DAQC2.fgLEVEL(self.addr, self.channel, self.level)
+        self.stop()
 
-    def initFG(self):
+    def start(self):
+        DAQC2.fgON(self.addr, 1)
+        # DAQC2.fgTYPE(self.addr, self.channel, self.type)
+        # DAQC2.fgLEVEL(self.addr, self.channel, self.level)
+        # DAQC2.fgFREQ(self.addr, self.channel, self.freq)
+    
+    def jog(self):
         DAQC2.fgON(self.addr, 1)
         DAQC2.fgTYPE(self.addr, self.channel, self.type)
         DAQC2.fgLEVEL(self.addr, self.channel, self.level)
@@ -36,46 +44,16 @@ class Stepper_Motor:
             print("Error: Frequency sensor not connected")
             return 0.0
 
-    def set_direction(self, direction: bool) -> bool:
-        try:
-            if direction:
-                DAQC2.setDOUTbit(0, self.dir_pin)
-            else:
-                DAQC2.clrDOUTbit(0, self.dir_pin)
-            return True
-        except:
-            print("error writing to motor prin")
-            return False
-
-    def set_clockwise(self):
-        self.set_direction(True)
-    
-    def set_counter_clockwise(self):
-        self.set_direction(False)
-
-    def set_motor(self, freq):
-        try:
-            DAQC2.setPWM(self.addr, self.channel, freq)
-            return True
-        except:
-            print("error writing to motor prin")
-            return False
-
-    def on(self):
-        DAQC2.clrDOUTbit(self.addr, 0)
-
-    def off(self):
-        DAQC2.setDOUTbit(self.addr, 0)
-
     def stop(self):
         DAQC2.fgOFF(self.addr, self.channel)
 
 if __name__ == '__main__':
     motor = Stepper_Motor()
-    motor.initFG()
-    print("setting rpm input")
-    desired_rpm = float(input("Enter desired rpm: "))
-    motor.set_rpm(desired_rpm)
+    motor.on()
+    print("starting on the motor for 30 seconds")
+    # print("setting rpm input")
+    # desired_rpm = float(input("Enter desired rpm: "))
+    # motor.set_rpm(desired_rpm)
     time.sleep(30)
     motor.stop()
 
